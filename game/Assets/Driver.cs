0531f1bc087b97d3004cd5889e29e591c4db60ae
MatthewDBrown50 <matthew.d.brown50@gmail.com>
2023-05-04 07:44:41 -0400

    Created player car and gave it basic controllable movement.

---
+++ game/Assets/Driver.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Driver : MonoBehaviour
+{
+    // SerializeField makes the field show up as an editible field in Unity
+    [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float steerSpeed = 200.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Time.deltaTime gives us the duration of each frame.
+        // Multiplying by this value gives us the same behavior across all systems, regardless of frame rate.
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+}
